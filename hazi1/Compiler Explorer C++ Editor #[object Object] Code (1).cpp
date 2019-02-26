e5680fcba7cd3f17bd7fa019015700a5812c9eba
zsofee444 <44588159+zsofee444@users.noreply.github.com>
2019-02-26 22:48:45 +0100

    Add files via upload

---
+++ hazi1/Compiler Explorer C++ Editor #[object Object] Code (1).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+
+double sqrt_newton(double num, double x0){
+    int n;
+    int i = 0;
+     do{
+         i = x0-(x0*x0-num)/2*x0;
+         i=x0;
+         ++i;
+    }
+    while(i <= n);
+return i;
+}
+
+int main(){
+    double num=612;
+    double x0=10;
+    int n=10;
+    sqrt_newton(num, x0);
+}
+
---
+++ hazi1/Compiler Explorer C++ Editor #[object Object] Code.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <math.h>
+
+//lineáris egyenlet megoldó:
+void solvelin(double a, double b){
+    if(a == 0.0){
+        std::cout <<"There is no soulution. \n";
+    }
+    else{
+        std::cout<< "the solution is:" << -b/a << "\n";
+    }
+}
+
+// másodfokú megoldó:
+void solve(double a, double b, double c){
+    if(a == 0.0 ){
+        solvelin(b, c);
+    }
+    else{
+        if(b*b -4*a*c < 0.0){
+            std::cout <<"There is no real solution. \n";
+        }
+        else if (b*b-4*a*c == 0.0 ){
+            std::cout <<"There is one solution, which is:" << -b/(2*a)<< "\n";
+        }
+        else(b*b-4*a*c > 0.0); {
+            std::cout << "There are two solutions, which are: \n"
+            << -b+sqrt(b*b-4*a*c)/(2*a) << "\n"
+            <<-b-sqrt(b*b-4*a*c)/(2*a) << "\n";
+        }
+    }
+}
+
+int main(){
+    double a=1 ;
+    double b=1 ;
+    double c=1 ;
+
+    solve( a,b,c);
+}
+
+ 
