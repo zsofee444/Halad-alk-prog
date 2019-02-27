61a5545c27e580a50eeec1c4f19720c668aee817
zsofee444 <44588159+zsofee444@users.noreply.github.com>
2019-02-27 15:07:55 +0100

    Update Compiler Explorer C++ Editor #[object Object] Code (1).cpp

--- hazi1/Compiler Explorer C++ Editor #[object Object] Code (1).cpp
+++ hazi1/Compiler Explorer C++ Editor #[object Object] Code (1).cpp
@@ -2,21 +2,19 @@
 #include <math.h>
 
 double sqrt_newton(double num, double x0){
-    int n;
+    int n=10;
     int i = 0;
      do{
-         i = x0-(x0*x0-num)/2*x0;
-         i=x0;
+         x= x0-(x0*x0-num)/2*x0;
          ++i;
     }
     while(i <= n);
-return i;
+return x;
 }
 
 int main(){
     double num=612;
     double x0=10;
-    int n=10;
     sqrt_newton(num, x0);
 }
 
